a856ebc1c6584ab4c708070d550bcf8f4664f4ba
loma-one <44441246+loma-one@users.noreply.github.com>
2025-01-19 11:22:49 +0100

    Add files via upload

---
+++ bookface_dark/bookfacedark.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Copyright (C) 2010-2024, the Friendica project
+ * SPDX-FileCopyrightText: 2010-2024 the Friendica project
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * Name: loma dunkel (in Bearbeitung)
+ * Licence: AGPL
+ * Author: Kristi H. @kmh@friendica.world feb @feb@loma.ml
+ * Overwrites: nav_bg, nav_icon_color, link_color, background_color, background_image, contentbg_transp
+ * Accented: No
+ */
+
+require_once 'view/theme/frio/php/PHPColors/Color.php';
+
+$accentColor = new Color($scheme_accent);
+
+$nav_bg = '#252728';
+$nav_icon_color = '#B0B3B8';
+$link_color = '#0866ff';
+$background_color = '#1C1C1D';
+$background_image = '';
+$contentbg_transp = 100;
+$menu_background_hover_color = 'rgba(255,255,255,.1)'; // rgba(255,255,255,.1)
+$font_color = '#B0B3B8';
+$font_color_darker = '#333';
+$border_color = '#333';
