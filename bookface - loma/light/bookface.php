3109d6f871aa4e418e607e239885c54997f6035d
loma-one <44441246+loma-one@users.noreply.github.com>
2025-01-22 19:00:42 +0100

    Add files via upload

---
+++ bookface - loma/light/bookface.php
@@ -0,0 +1,30 @@
+ <?php
+/*
+ * Copyright (C) 2010-2024, the Friendica project
+ * SPDX-FileCopyrightText: 2010-2024 the Friendica project
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * Name: loma hell (light)
+ * Licence: AGPL
+ * Author: Kristi H. @kmh@friendica.world feb @feb@loma.ml
+ * Overwrites: nav_bg, nav_icon_color, link_color, background_color, background_image, contentbg_transp
+ * Accented: No
+ */
+
+require_once 'view/theme/frio/php/PHPColors/Color.php';
+
+$accentColor = new Color($scheme_accent);
+
+$nav_bg = '#f2f4f7';
+$nav_icon_color = '#606637';
+$link_color = '#0866ff';
+$background_color = '#f2f4f7';
+$background_image = '';
+$contentbg_transp = 100;
+$menu_background_hover_color = '#8080801a'; // rgba(128,128,128,.1)
+$font_color = '#313131';
+$font_color_darker = '#333';
+$border_color = '#eee';
+
+
